b909cfc940064180be9dc54f5737bf346c0b3b21
Anish-Singha <129192536+Anish-Singha@users.noreply.github.com>
2023-03-31 15:38:02 +0530

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+//import "../st"
+
+const Header = () => {
+  return (
+    <header>
+      {" "}
+      <h1> THE NOTEPAD </h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>Title </h1>
+      <p>write notes</p>
+    </div>
+  );
+};
+export default Note;
